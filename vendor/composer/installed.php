77351e984995e4ba59876c959c7c5c5041b7eae8
Roel van Duijnhoven <roel@vanduijnhoven.com>
2021-01-31 20:59:31 +0100

    Commit vendor.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '683f795e4f402f2ec55bc51c90769f220d9eeae3',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '683f795e4f402f2ec55bc51c90769f220d9eeae3',
+    ),
+  ),
+);
